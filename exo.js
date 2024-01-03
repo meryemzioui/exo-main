4bfebd603a480d9aedea75e567f30957b6cb7804
meryem zioui <meryem.zioui@gmail.com>
2024-01-03 11:02:42 +0100

    first commit

---
+++ exo.js
@@ -0,0 +1,48 @@
+// var produit=["element1","element2","element3"]
+// for (i=0;i<produit.length;i++)
+// {
+//     console.log(produit[i])
+// }
+
+// // moyenne
+
+// var point=[10,15,10];
+// somme=0;
+// for ( i=0;i<point.length;i++)
+// {
+//      somme=somme + point[i];
+// }
+// var moyenne=somme/point.length
+
+//     console.log("la moyenne est : ",moyenne)
+
+// valeur min
+// var point = [10, 15, 11, 5, 0, 20];
+// var min = point[0];
+// for (i = 1; i < point.length; i++) {
+//   if (min > point[i]) {
+//     min = point[i];
+//   }
+// }
+// console.log("valeur minimale : ", min);
+// var max = point[0];
+// for (i = 1; i < point.length; i++) {
+//   if (max < point[i]) {
+//     max = point[i];
+//   }
+// }
+// console.log("valeur mmaximale : ", max);
+
+// object
+// var personne = {
+//   firstname: "zioui",
+//   lastname: "meryem",
+//   age: 36,
+// };
+
+// function plusage(a) {
+//   return a.age+5;
+// }
+// console.log(plusage(personne)
+// );
+
